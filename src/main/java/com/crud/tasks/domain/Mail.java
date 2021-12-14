c339d6991e76bb4cdcb959b3e871430771cce653
baalperez <baal_perez@wp.pl>
2021-12-14 23:19:27 +0100

    23.3 CC in mail

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,12 @@
+package com.crud.tasks.domain;
+
+import lombok.*;
+
+@Getter
+@Builder
+public class Mail {
+    private final String mailTo;
+    private final String message;
+    private final String subject;
+    private final String toCc;
+}
