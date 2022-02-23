03bacdefc624bb62479d1aa4a9255bac211e22e4
baalperez <baal_perez@wp.pl>
2022-02-23 13:08:28 +0100

    32.2 - COMPANY_DETAILS

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -1,9 +1,9 @@
 package com.crud.tasks.scheduler;
 
-import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.domain.Mail;
 import com.crud.tasks.repository.TaskRepository;
 import com.crud.tasks.service.SimpleEmailService;
+import com.crud.tasks.trello.config.AdminConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
--- src/main/java/com/crud/tasks/trello/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/trello/config/AdminConfig.java
@@ -1,4 +1,4 @@
-package com.crud.tasks.config;
+package com.crud.tasks.trello.config;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,4 +9,22 @@ import org.springframework.stereotype.Component;
 public class AdminConfig {
     @Value("${admin.mail}")
     private String adminMail;
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${info.app.company.name}")
+    private String companyName;
+
+    @Value("${info.app.company.email}")
+    private String companyEmail;
+
+    @Value("${info.app.company.phone}")
+    private String companyPhone;
+
+    public String viewCompanyDetails() {
+        return companyName + '\'' +
+                companyEmail + '\'' +
+                companyPhone + '\'';
+    }
 }
